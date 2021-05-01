b1e28b9c1b2fd56e5c8f4e72ec6e40f7dc67abfb
filon13p <filon13p@gmail.com>
2021-05-01 13:13:28 +0300

    Все равботает

--- src/test/java/Tree/BTree/BTreeTest.java
+++ src/test/java/Tree/BTree/BTreeTest.java
@@ -10,10 +10,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BTreeTest {
-    BTree<String> tree;
+    BTree<Integer> tree;
     @BeforeEach
-    public  void init(){
-        tree = new BTree<String>(3);
+    public  void init()
+    {
+        tree = new BTree<Integer>(3);
+
+
     }
     @Nested
     class ConstructTests{
@@ -24,19 +27,19 @@ class BTreeTest {
 
         @Test
         void testInsert_WhenTreeIsEmpty_ThenReturnTrue() {
-            String val = "value";
 
-            tree.insert( val);
+
+            tree.insert(1);
 
         }
 
         @Disabled
         @ParameterizedTest
         @ValueSource(ints = {2,0,-2,Integer.MAX_VALUE,Integer.MIN_VALUE})
-        void testInsert_WhenTreeIsEmptyWithDifferentKeys_ThenReturnTrue(Integer Key) {
-            String val = "value";
+        void testInsert_WhenTreeIsEmptyWithDifferentKeys_ThenReturnTrue(Integer key) {
 
-           tree.insert( val);
+
+           tree.insert( key);
         }
 
 
@@ -47,6 +50,127 @@ class BTreeTest {
     }
     @Nested
     class FindTests{
+
+    }
+
+    @Nested
+    class PutTests{
+        @ParameterizedTest
+        @ValueSource(ints = {2, 0, -2, Integer.MAX_VALUE, Integer.MIN_VALUE})
+        void testPutVal_WhenAndPut1Val_ThenShouldBeOk(Integer val) {
+
+            tree.insert(val);
+            Node<Integer> newNode =   tree.Root;
+            assertEquals(val, newNode.Keys.get(0));
+
+        }
+
+        @Test
+        void testPutVal_WhenPutManyValLessThenRank_ThenShouldBeOk() {
+
+            tree.insert(1);
+            tree.insert(2);
+            Node<Integer> newNode =   tree.Root;
+            assertAll(
+                    () -> assertEquals((Integer) 1, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(1))
+            );
+        }
+
+        @Test
+        void testSplit_123() {
+
+             tree.insert(1);
+             tree.insert(2);
+             tree.insert(3);
+            Node<Integer> newNode =   tree.Root;
+            assertAll(
+
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0))
+            );
+        }
+        @Test
+        void testSplit_1234() {
+             tree.insert(1);
+             tree.insert(2);
+             tree.insert(3);
+             tree.insert(4);
+            Node<Integer> newNode =   tree.Root;
+            assertAll(
+
+
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0)),
+                    () -> assertEquals((Integer) 4, newNode.Childes.get(1).Keys.get(1))
+            );
+
+
+
+        }
+        @Test
+        void testSplit_12345() {
+             tree.insert(1);
+             tree.insert(2);
+             tree.insert(3);
+             tree.insert(4);
+             tree.insert(5);
+            Node<Integer> newNode =   tree.Root;
+            assertAll(
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 4, newNode.Keys.get(1)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0)),
+                    () -> assertEquals((Integer) 5, newNode.Childes.get(2).Keys.get(0)),
+                    () -> assertEquals( 3, newNode.Childes.size())
+            );
+        }
+        @Test
+        void testSplit_123456() {
+             tree.insert(1);
+             tree.insert(2);
+             tree.insert(3);
+             tree.insert(4);
+             tree.insert(5);
+             tree.insert(6);
+            Node<Integer> newNode =   tree.Root;
+            assertAll(
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 4, newNode.Keys.get(1)),
+                    () -> assertEquals((Integer) 5, newNode.Childes.get(2).Keys.get(0)),
+                    () -> assertEquals((Integer) 6, newNode.Childes.get(2).Keys.get(1)),
+                    () -> assertEquals( 3, newNode.Childes.size())
+            );
+        }
+        @Test
+        void testSplit_1234567() {
+             tree.insert(1);
+             tree.insert(2);
+             tree.insert(3);
+             tree.insert(4);
+             tree.insert(5);
+             tree.insert(6);
+             tree.insert(7);
+            Node<Integer> newNode =   tree.Root;
+            Node<Integer> Child2 = newNode.Childes.get(0);
+            Node<Integer> Child6 = newNode.Childes.get(1);
+
+
+            assertAll(
+                    () -> assertEquals((Integer) 4, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 2, Child2.Keys.get(0)),
+                    () -> assertEquals((Integer) 6, Child6.Keys.get(0)),
+                    () -> assertEquals((Integer) 1, Child2.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 3, Child2.Childes.get(1).Keys.get(0)),
+                    () -> assertEquals((Integer) 5, Child6.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 7, Child6.Childes.get(1).Keys.get(0))
+
+            );
+        }
 
     }
 
--- src/test/java/Tree/BTree/NodeTest.java
+++ src/test/java/Tree/BTree/NodeTest.java
@@ -41,28 +41,91 @@ class NodeTest {
         }
 
         @Test
-        void testPutVal_WhenPutManyValMoreThenRank_ThenNodeShouldSplit() {
+        void testSplit_123() {
             newNode.put(1);
             newNode.put(2);
-
             newNode.put(3);
 
             assertAll(
 
-                    () -> assertNotNull(newNode.Childes.get(0)),
-                    () -> assertNotNull(newNode.Childes.get(1)),
-                    () -> assertEquals((Integer) 2, newNode.Keys.get(0))
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0))
+            );
+        }
+        @Test
+        void testSplit_1234() {
+            newNode.put(1);
+            newNode.put(2);
+            newNode.put(3);
+            newNode.put(4);
+
+            assertAll(
+
+
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0)),
+                    () -> assertEquals((Integer) 4, newNode.Childes.get(1).Keys.get(1))
+            );
+
+
+
+        }
+        @Test
+        void testSplit_12345() {
+            newNode.put(1);
+            newNode.put(2);
+            newNode.put(3);
+            newNode.put(4);
+            newNode.put(5);
+
+
+
+            assertAll(
+
+
+                    () -> assertEquals((Integer) 1, newNode.Childes.get(0).Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 2, newNode.Keys.get(0)),
+                    () -> assertEquals((Integer) 3, newNode.Childes.get(1).Keys.get(0)),
+                    () -> assertEquals((Integer) 4, newNode.Childes.get(1).Keys.get(1))
+                    () -> assertEquals( 3, newNode.Childes.size())
+
             );
         }
     }
 
+    @Test
+    void testGetChildIndex_ThenReturnLeftmostChidl() {
 
+        newNode.put(2);
+        newNode.put(3);
+        newNode.put(4);
+
+        int actual =  newNode.getChildIndex(1);
+        assertEquals(0, actual);
+
+    }
 
     @Test
-    void testGetChildIndex() {
+    void testGetChildIndex_ThenReturnRightmostChidl() {
         newNode.put(1);
         newNode.put(2);
         newNode.put(3);
+
+        int actual =  newNode.getChildIndex(4);
+        assertEquals(1, actual);
+
+    }
+    @Test
+    void testGetChildIndex_ThenReturnMiddleChidl() {
+        newNode.put(1);
+        newNode.put(2);
+        newNode.put(3);
+        newNode.put(5);
+        newNode.put(6);
+
 
         int actual =  newNode.getChildIndex(4);
         assertEquals(1, actual);
